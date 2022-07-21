336ef8678da94a6bf34893b575abe1184b2353ba
Luiz Guerra <luizpedrofg@gmail.com>
2022-07-21 01:03:08 -0300

    UIBezierPath code example

---
+++ BezierPath/SwiftUI Path/SwiftUI Path/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Path
+//
+//  Created by Luiz Pedro Franciscatto Guerra on 20/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
